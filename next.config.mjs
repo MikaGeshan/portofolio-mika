0f75529ae1d76ebb3755a8990229f3521ba0919f
mika.arip <mika.arip@quadrant-si.id>
2025-12-16 11:27:55 +0700

    disable turbopack

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,8 @@
 const nextConfig = {
   webpack(config) {
     // Grab the existing rule that handles SVG imports
-    const fileLoaderRule = config.module.rules.find((rule) =>
-      rule.test?.test?.(".svg")
+    const fileLoaderRule = config.module.rules.find(
+      (rule) => rule.test instanceof RegExp && rule.test.test(".svg")
     );
 
     config.module.rules.push(
@@ -29,6 +29,7 @@ const nextConfig = {
 
     return config;
   },
+  turbopack: false,
 };
 
 export default nextConfig;
